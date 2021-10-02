fec52c322d5f731606c2200382aa09f455ef6fef
REVE <kgw1988@naver.com>
2021-10-02 11:32:49 +0900

    add favicon

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -4,7 +4,8 @@ module.exports = {
 	dest: 'docs',
 	base: '/blog/',
 	head: [
-		['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}]
+		['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}],
+		['link', {rel: 'icon', href: 'https://user-images.githubusercontent.com/9425680/135701075-eeb71836-082f-4fc5-b8eb-9aa3cd75af17.png'}]
 	],
 	markdown: {
 		lineNumbers: true,
