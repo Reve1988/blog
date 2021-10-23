6b5d4a8f7a56b987d092ec7740791ef2236da20a
REVE <kgw1988@naver.com>
2021-10-23 14:35:56 +0900

    modify google analytics config

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -5,7 +5,9 @@ module.exports = {
 	base: '/blog/',
 	head: [
 		['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}],
-		['link', {rel: 'icon', href: 'https://user-images.githubusercontent.com/9425680/135701075-eeb71836-082f-4fc5-b8eb-9aa3cd75af17.png'}]
+		['link', {rel: 'icon', href: 'https://user-images.githubusercontent.com/9425680/135701075-eeb71836-082f-4fc5-b8eb-9aa3cd75af17.png'}],
+		["script", {async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"}],
+		["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-R056XQ43KK');"]],
 	],
 	markdown: {
 		lineNumbers: true,
@@ -25,14 +27,6 @@ module.exports = {
 					}
 				]
 			},
-		],
-		[
-			[
-				'@vuepress/google-analytics',
-				{
-					'ga': 'G-R056XQ43KK'
-				}
-			]
 		],
 	],
 }
