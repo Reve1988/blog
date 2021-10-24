ed78c984c6d7640bd48bb538dd0832dc77ac6962
REVE <kgw1988@naver.com>
2021-10-24 11:59:09 +0900

    modify base url

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: 'Revelope',
 	description: 'Developer Reve\'s Blog',
 	dest: 'docs',
-	base: '/blog/',
+	base: '/',
 	head: [
 		['meta', {name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'}],
 		['link', {rel: 'icon', href: 'https://user-images.githubusercontent.com/9425680/135701075-eeb71836-082f-4fc5-b8eb-9aa3cd75af17.png'}],
