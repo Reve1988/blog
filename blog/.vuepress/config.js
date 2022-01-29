189919a4091af2e3a561fdbd5858fbc0bbca578b
REVE <kgw1988@naver.com>
2022-01-29 13:08:06 +0900

    add locale option

--- blog/.vuepress/config.js
+++ blog/.vuepress/config.js
@@ -9,6 +9,9 @@ module.exports = {
 		["script", {async: true, src: "https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"}],
 		["script", {}, ["window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-R056XQ43KK');"]],
 	],
+	locales: {
+		'/': {lang: 'ko-KR'}
+	},
 	markdown: {
 		lineNumbers: true,
 	},
